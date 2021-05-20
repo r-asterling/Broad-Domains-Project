228fbb3531fe3e86bc9f63c5ee6169373e110639
Rhiannon Asterling <84334856+r-asterling@users.noreply.github.com>
2021-05-20 15:49:28 +0100

    Create 01-trimmed_kang_rpe1_fastqc_runner.sh

---
+++ 01-FASTQC/RPE1 Cells/Trimmed/01-trimmed_kang_rpe1_fastqc_runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540133_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540134_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540135_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540136_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540137_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540138_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540157_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540158_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540159_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540160_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540161_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540162_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540163_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540164_trimmed.fastq.gz
+sbatch 01-trimmed_kang_rpe1_fastqc.sh SRR10540165_trimmed.fastq.gz
